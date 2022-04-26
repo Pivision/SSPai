efb2b356341f64bcf5ba1343728a07db7f387c30
Pivision <6907450@qq,com>
2022-04-26 18:57:35 +0800

    fix: correct navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+window.onload = function () {
+    var $li = $(".headTab");
+    $li.mouseenter(function () {
+        $(this).children("ul").show();
+    });
+
+    $li.mouseleave(function () {
+        $(this).children("ul").hide();
+    });
+};
+
 function listBtn(display, text, display2, r1, r2, color1, color2) {
     var listDown = document.getElementsByClassName('listDown');
     for (var i = 0; i < listDown.length; i++) {
@@ -12,85 +23,6 @@ function listBtn(display, text, display2, r1, r2, color1, color2) {
     document.querySelector(".listBtn1 > input:last-child").style.color = color2;
     document.querySelector(".listBtn1 > input").style.background = color2;
     document.querySelector(".listBtn1 > input").style.color = color1;
-}
-
-window.onload = function () {
-    var head = document.querySelector(".htab");
-    var head1 = document.querySelector(".headTab1");
-    var head2 = document.querySelector(".headTab2");
-    var head3 = document.querySelector(".headTab3");
-    var head4 = document.querySelector(".headTab4");
-    var head5 = document.querySelector(".headTab5");
-    var headb = document.querySelector(".htabBottom");
-    var headli1 = document.querySelector(".headLi1");
-    var headli2 = document.querySelector(".headLi2");
-    head.onmouseout = headTab;
-    head1.onmouseover = headTab1;
-    head2.onmouseover = headTab2;
-    head3.onmouseover = headTab3;
-    head4.onmouseover = headTab4;
-    head5.onmouseover = headTab5;
-    headb.onmouseover = headTabb;
-    headli1.onmouseover = headL1;
-    headli1.onmouseout = headL01;
-    headli2.onmouseover = headL2;
-    headli2.onmouseout = headL02;
-}
-
-headTab = function () {
-    var htabBottom = document.getElementsByClassName('htabBottom');
-    for (var i = 0; i < htabBottom.length; i++) {
-        htabBottom[i].style.display = 'none';
-    };
-}
-
-headTab1 = function () {
-    document.querySelector(".htabBottom").style.display = 'block';
-    document.querySelector(".headLi1 > a").innerText = '社区广场';
-    document.querySelector(".headLi2 > a").innerText = '一派';
-    document.querySelector(".headLi1 > a").href = '#';
-}
-
-headTab2 = function () {
-    document.querySelector(".htabBottom").style.display = 'none';
-}
-
-headTab3 = function () {
-    document.querySelector(".htabBottom").style.display = 'block';
-    document.querySelector(".headLi1 > a").innerText = '话题广场';
-    document.querySelector(".headLi2 > a").innerText = '相机陈列馆';
-    document.querySelector(".headLi1 > a").href = 'list.html';
-}
-
-headTab4 = function () {
-    document.querySelector(".htabBottom").style.display = 'block';
-    document.querySelector(".headLi1 > a").innerText = '正版软件';
-    document.querySelector(".headLi2 > a").innerText = '官方淘宝';
-    document.querySelector(".headLi1 > a").href = '#';
-}
-
-headTab5 = function () {
-    document.querySelector(".htabBottom").style.display = 'none';
-}
-
-headTabb = function () {
-    this.style.display = 'block';
-}
-
-headL1 = function () {
-    this.style.background = 'rgba(0, 0, 0, 0.25)';
-}
-
-headL01 = function () {
-    this.style.background = 'rgba(0, 0, 0, 0)';
-}
-
-headL2 = function () {
-    this.style.background = 'rgba(0, 0, 0, 0.25)';
-}
-
-headL02 = function () {
-    this.style.background = 'rgba(0, 0, 0, 0)';
 }
 
 var a = 0;
